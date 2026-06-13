adac656293d14f33e68573735fe977cf6e8e46b1
Tobstron <fawoletobiloba184@gmail.com>
2026-06-13 15:38:03 +0100

    week5 pt2

---
+++ chamber/scripts/places.js
@@ -0,0 +1,50 @@
+export const places = [
+{
+    name: "Alausa Secretariat",
+    image: "images/alausa.webp",
+    address: "Alausa, Ikeja, Lagos",
+    description: "Administrative center of Lagos State government offices."
+},
+{
+    name: "Johnson Jakande Tinubu Park",
+    image: "images/jjtpark.webp",
+    address: "Alausa, Ikeja, Lagos",
+    description: "Popular relaxation and event venue in Ikeja."
+},
+{
+    name: "Computer Village",
+    image: "images/computervillage.webp",
+    address: "Otigba, Ikeja",
+    description: "Largest ICT and electronics market in Nigeria."
+},
+{
+    name: "Maryland Interchange",
+    image: "images/maryland.webp",
+    address: "Maryland, Lagos",
+    description: "Major transportation hub connecting Lagos districts."
+},
+{
+    name: "Ikeja City Mall",
+    image: "images/ikejamall.webp",
+    address: "Obafemi Awolowo Way",
+    description: "Premier shopping and entertainment center."
+},
+{
+    name: "Murtala Muhammed Airport",
+    image: "images/airport.webp",
+    address: "Airport Road, Ikeja",
+    description: "Nigeria's busiest international airport."
+},
+{
+    name: "Ndubuisi Kanu Park",
+    image: "images/ndubuisi.webp",
+    address: "Mobolaji Johnson Avenue",
+    description: "Family-friendly park for recreation and events."
+},
+{
+    name: "Kalakuta Museum",
+    image: "images/kalakuta.webp",
+    address: "Gbemisola Street, Ikeja",
+    description: "Museum dedicated to the life of Fela Kuti."
+}
+];
